177f5be78f815366eb264a0299f0f0a32474bebd
Andreas Baumbach <andreas.baumbach@kip.uni-heidelberg.de>
2017-11-16 15:46:56 +0100

    activate hel implementation

--- jobcontrol.py
+++ jobcontrol.py
@@ -7,12 +7,14 @@ Each job is contained in a bash file
 """
 from __future__ import division, print_function
 
-from cluster import bwnemo
+from cluster import bwnemo, hel
 
 
 def execute(jobs, clustername, parameters):
     if clustername == 'BWnemo':
         bwnemo.execute(jobs, **parameters)
+    elif clustername == 'hel':
+        hel.execute(jobs, **parameters)
     else:
         raise NotImplementedError()
 
